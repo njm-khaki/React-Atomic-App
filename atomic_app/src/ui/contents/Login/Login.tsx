4ba19a50b62d414ca70d5001f786e18a90a3a32c
njm-khaki <39180345+s1523119@users.noreply.github.com>
2022-06-26 16:24:34 +0900

    rename login -> Login

---
+++ atomic_app/src/ui/contents/Login/Login.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import MainContents from './templates/MainContents';
+
+/**
+ * ログイン画面本体
+ * @returns 
+ */
+const Login: React.FC = () => {
+    return (
+        <React.Fragment>
+            {/* ナビゲーションバー */}
+            <NavigationBar />
+            {/* ログイン画面本体のレイアウト */}
+            <MainContents />
+        </React.Fragment>
+    );
+}
+
+export default Login;
